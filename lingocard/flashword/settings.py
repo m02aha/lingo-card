74ee3c1e7ef2fd4094325640806bf24c7708c6d4
Maha Faiz <65115786+m02aha@users.noreply.github.com>
2025-04-01 10:44:55 +0200

    Update settings.py

--- lingocard/flashword/settings.py
+++ lingocard/flashword/settings.py
@@ -126,9 +126,10 @@ USE_TZ = True  # This should be True to enable timezone support
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    BASE_DIR/'static',
-]
+
+# Define the STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+
 
 #upload files to this folder
 MEDIA_ROOT=BASE_DIR / "uploads"

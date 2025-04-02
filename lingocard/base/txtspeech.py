dee9ed764b866f354b88a8915228dc4568b7741a
Maha Faiz <65115786+m02aha@users.noreply.github.com>
2025-04-02 09:50:46 +0200

    Update txtspeech.py
    editing the storage to be uploded to dropbx insteadof media storage

--- lingocard/base/txtspeech.py
+++ lingocard/base/txtspeech.py
@@ -2,15 +2,15 @@ from gtts import gTTS
 import uuid
 import os
 from django.conf import settings
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 
 def txtToSpeach(text,lang):
     tts=gTTS(text=text,lang=lang,slow=False)
     filename=f"tts_audio_{uuid.uuid4()}.mp3"
-    audio_path=os.path.join(settings.MEDIA_ROOT,'audio',filename)
-    # Ensure the directory exists
-    os.makedirs(os.path.dirname(audio_path), exist_ok=True)
-    tts.save(audio_path)
-    
+     # Save the audio to a ContentFile and then to Dropbox
+    audio_content = ContentFile(tts.save(filename))
+    audio_path = default_storage.save(f'audio/{filename}', audio_content)
 
     return audio_path
 

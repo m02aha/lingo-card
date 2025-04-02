8eda22d15e6ba4045a0cfa2c49432df5dd617463
Maha Faiz <65115786+m02aha@users.noreply.github.com>
2025-04-02 09:59:55 +0200

    Update urls.py

--- lingocard/base/urls.py
+++ lingocard/base/urls.py
@@ -17,5 +17,5 @@ urlpatterns=[
 ]
 # Serve media files during development
 
-if settings.DEBUG:
-    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

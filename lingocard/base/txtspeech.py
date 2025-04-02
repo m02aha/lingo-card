6f23059e1760add03afe7093d7251a7c55a7bde5
Maha Faiz <65115786+m02aha@users.noreply.github.com>
2025-04-02 17:38:11 +0200

    Update txtspeech.py

--- lingocard/base/txtspeech.py
+++ lingocard/base/txtspeech.py
@@ -1,10 +1,8 @@
+import dropbox
 from gtts import gTTS 
 import uuid
+import io
 import os
-from django.conf import settings
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
-import  io
 
 def txtToSpeach(text, lang):
     tts = gTTS(text=text, lang=lang, slow=False)
@@ -15,9 +13,10 @@ def txtToSpeach(text, lang):
     audio_buffer.seek(0)  # Move to the beginning of the BytesIO buffer
 
     filename = f"tts_audio_{uuid.uuid4()}.mp3"
-    
-    # Save the audio to Dropbox using the default storage
-    audio_path = default_storage.save(f'audio/{filename}', ContentFile(audio_buffer.read()))
+    dbx = dropbox.Dropbox(os.environ.get('DROPBOX_ACCESS_TOKEN'))
 
-    return audio_path
+    # Upload the audio file to Dropbox
+    dbx.files_upload(audio_buffer.read(), f'/lingo_card/audio/{filename}')
+
+    return f'/lingo_card/audio/{filename}'  # Return the path to the file
 # print(txtToSpeach('hello every one','en'))
